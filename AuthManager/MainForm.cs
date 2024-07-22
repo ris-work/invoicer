edc9b02224a3108f2bf258fb2e4116adb089d118
rishi-on-desktop <oss@rishikeshan.com>
2024-07-22 21:37:31 +0000

    Sample work
    FossilOrigin-Name: 9c64429b3d744bc8e8ac0e2cf32d3b7421c4235521568fb4ecf71884898d7fa0

--- AuthManager/MainForm.cs
+++ AuthManager/MainForm.cs
@@ -1,7 +1,10 @@
 using Eto.Drawing;
 using Eto.Forms;
 using System;
+using System.Configuration;
+using System.Linq;
 using Tomlyn;
+using System.Collections.Generic;
 
 namespace AuthManager
 {
@@ -9,6 +12,24 @@ namespace AuthManager
     {
         public MainForm()
         {
+            var UserList = new GridView();
+            UserList.Columns.Add(new GridColumn { HeaderText = "UserID", DataCell = new TextBoxCell(0) });
+            UserList.Columns.Add(new GridColumn { HeaderText = "Username", DataCell = new TextBoxCell(1) });
+            UserList.Columns.Add(new GridColumn { HeaderText = "Modified", DataCell = new TextBoxCell(2) });
+            UserList.Columns.Add(new GridColumn { HeaderText = "Created", DataCell = new TextBoxCell(3) });
+            string x="";
+            using (var ctx = new NewinvContext())
+            {
+                var users = ctx.Credentials.ToList();
+                var list = new List<GridItem>();
+                
+                foreach (var item in users)
+                {
+                    list.Add(new GridItem(item.Userid, item.Username, item.Modified, item.Created.ToString()));
+                }
+                UserList.DataStore = list;
+
+            }
             Title = "My Eto Form";
             MinimumSize = new Size(200, 200);
 
@@ -18,7 +39,9 @@ namespace AuthManager
                 Items =
                 {
                     "Hello World!",
+                    x,
                     new Button((e, a) => MessageBox.Show("Hello")){  },
+                    UserList
 
                 }
 
