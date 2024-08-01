46f3e5fde5f88a38647ad4240c65b4a994ed1136
rishi-on-desktop <oss@rishikeshan.com>
2024-08-01 03:46:49 +0000

    PosTerminal and SearchDialog
    FossilOrigin-Name: 32edf6fdd9ba79da86fea64ea00b7e2e44ba0a79db18dce69d75df0a00961d1a

---
+++ EtoFE/PosTerminal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eto.Drawing;
+using Eto.Forms;
+
+namespace EtoFE
+{
+    public class PosTerminal: Form
+    {
+        public PosTerminal()
+        {
+            TableLayout TL = new TableLayout();
+            Label LabelPendingInvoices = new Label() { Text = "Pending invoices:" };
+            ListBox PendingInvoices = new ListBox();
+            Label LabelPastSuccessfulInvocies = new Label() { Text = "Successful invoices:" };
+            ListBox PastSuccessfulInvoices = new ListBox() {  };
+            Label LabelBarcode = new Label() { Text = "Barcode: " };
+            TextBox Barcode = new TextBox();
+            Label CurrentItem = new Label();
+            Label LabelPrice = new Label() { Text = "Price: " };
+            TextBox Price = new TextBox();
+            TextBox Quantity = new TextBox();
+            Label LabelQuantity = new Label() { Text = "Quantity: " };
+            Label LabelVatCategory = new Label() { Text = "Vat category:" };
+            TextBox VatCategory = new TextBox();
+            Label CurrentVatRate = new Label();
+            Label CurrentVatCategory = new Label();
+
+            TL.Rows.Add(new TableRow(LabelPendingInvoices, LabelPastSuccessfulInvocies));
+            TL.Rows.Add(new TableRow(PendingInvoices, PastSuccessfulInvoices));
+            TL.Rows.Add(new TableRow(LabelBarcode, Barcode, CurrentItem));
+            TL.Rows.Add(new TableRow(LabelPrice, Price));
+            TL.Rows.Add(new TableRow(LabelQuantity, Quantity));
+            TL.Rows.Add(new TableRow(LabelVatCategory, VatCategory, CurrentVatRate, CurrentVatCategory));
+            Content = TL;
+        }
+    }
+}
---
+++ EtoFE/SearchDialog.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eto.Forms;
+using Eto.Drawing;
+using RV.InvNew.Common;
+
+namespace EtoFE
+{
+    public class SearchDialog: Dialog
+    {
+        public SearchDialog(List<PosCatalogue> PC)
+        {
+            TableLayout TL = new TableLayout();
+            Label SL = new Label() { Text = "Search for: " };
+            Label LabelResults = new Label() { Text = "Results: " };
+            GridView Results = new GridView();
+            Results.Columns.Add(new GridColumn { HeaderText = "Itemcode", DataCell = new TextBoxCell(0) });
+            Results.Columns.Add(new GridColumn { HeaderText = "Description", DataCell = new TextBoxCell(1) });
+            TextBox SearchBox = new TextBox();
+            SearchBox.KeyUp += (e, a) =>
+            {
+                if (SearchBox.Text.Length > 3)
+                {
+                    bool searching = true;
+                    (new Thread(() =>
+                    {
+                        List<PosCatalogue> Filtered = PC.Where((x) => x.itemdesc.Contains(SearchBox.Text)).Take(100).ToList();
+                        List<GridItem> GI = new List<GridItem>();
+                        foreach (var item in Filtered)
+                        {
+                            GI.Add(new GridItem(item.itemcode, item.itemdesc) { });
+                        }
+                        searching = false;
+                    })).Start();
+                }
+            };
+
+
+            Content = TL;
+        }
+    }
+}
