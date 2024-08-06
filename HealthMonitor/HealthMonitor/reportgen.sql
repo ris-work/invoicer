37df385d40d2047f76c76061fa24b45116199cdb
rishi-on-desktop <oss@rishikeshan.com>
2024-08-06 12:27:20 +0000

    SQL script for viewing details (HealthMonitor)
    FossilOrigin-Name: ff00a9b99e0fc418ec56b5f9fa5cfa5637d249814923daa47f4e589a8d6856c7

---
+++ HealthMonitor/HealthMonitor/reportgen.sql
@@ -0,0 +1,5 @@
+--Decaminute latency averages
+WITH candidates AS (SELECT DISTINCT(dest) AS dest FROM pings WHERE dest LIKE '192%'), stats_decaminutes AS (SELECT substr(time_now, 1, 15) AS decaminute, AVG(latency) AS latency_average, count(latency) AS ping_counts, dest, SUM(was_it_ok_not_corrupt)/COUNT(was_it_ok_not_corrupt) AS success_rate FROM pings GROUP BY decaminute, dest) SELECT * FROM candidates can JOIN stats_decaminutes sd ON can.dest = sd.dest WHERE ping_counts > 10 ORDER BY latency_average DESC LIMIT 50;
+
+--Success rates per decaminute
+WITH candidates AS (SELECT DISTINCT(dest) AS dest FROM pings WHERE dest LIKE '192%'), stats_decaminutes AS (SELECT substr(time_now, 1, 15) AS decaminute, AVG(latency) AS latency_average, count(latency) AS ping_counts, dest, SUM(was_it_ok_not_corrupt)/COUNT(was_it_ok_not_corrupt) AS success_rate FROM pings GROUP BY decaminute, dest) SELECT * FROM candidates can JOIN stats_decaminutes sd ON can.dest = sd.dest WHERE ping_counts > 10 ORDER BY success_rate LIMIT 50;
