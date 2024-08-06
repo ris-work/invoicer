d8eda4992d2228340b0553a302901a9b3f2e56f0
rishi-on-desktop <oss@rishikeshan.com>
2024-08-06 18:39:24 +0000

    Worst 5 by IP
    FossilOrigin-Name: 9f876cf5edcec2152bd42d068cb792b4df634b1c929015a08478c8826336461b

--- HealthMonitor/HealthMonitor/reportgen.sql
+++ HealthMonitor/HealthMonitor/reportgen.sql
@@ -1,5 +1,16 @@
 --Decaminute latency averages
-WITH candidates AS (SELECT DISTINCT(dest) AS dest FROM pings WHERE dest LIKE '192%'), stats_decaminutes AS (SELECT substr(time_now, 1, 15) AS decaminute, AVG(latency) AS latency_average, count(latency) AS ping_counts, dest, SUM(was_it_ok_not_corrupt)/COUNT(was_it_ok_not_corrupt) AS success_rate FROM pings GROUP BY decaminute, dest) SELECT * FROM candidates can JOIN stats_decaminutes sd ON can.dest = sd.dest WHERE ping_counts > 10 ORDER BY latency_average DESC LIMIT 50;
+WITH candidates AS (SELECT DISTINCT(dest) AS dest FROM pings WHERE dest LIKE '192%'), 
+stats_decaminutes AS (SELECT substr(time_now, 1, 15) AS decaminute, AVG(latency) AS latency_average, count(latency) AS ping_counts, 
+    dest, SUM(was_it_ok_not_corrupt)/COUNT(was_it_ok_not_corrupt) AS success_rate 
+    FROM pings GROUP BY decaminute, dest),
+limited_to_5_worst AS (SELECT *, row_number() OVER (PARTITION BY dest ORDER BY latency_average DESC) AS group_rn FROM stats_decaminutes)
+SELECT * FROM candidates can JOIN limited_to_5_worst sd ON can.dest = sd.dest WHERE ping_counts > 10 AND group_rn < 5 ORDER BY latency_average DESC;
 
 --Success rates per decaminute
 WITH candidates AS (SELECT DISTINCT(dest) AS dest FROM pings WHERE dest LIKE '192%'), stats_decaminutes AS (SELECT substr(time_now, 1, 15) AS decaminute, AVG(latency) AS latency_average, count(latency) AS ping_counts, dest, SUM(was_it_ok_not_corrupt)/COUNT(was_it_ok_not_corrupt) AS success_rate FROM pings GROUP BY decaminute, dest) SELECT * FROM candidates can JOIN stats_decaminutes sd ON can.dest = sd.dest WHERE ping_counts > 10 ORDER BY success_rate LIMIT 50;
+WITH candidates AS (SELECT DISTINCT(dest) AS dest FROM pings WHERE dest LIKE '192%'), 
+stats_decaminutes AS (SELECT substr(time_now, 1, 15) AS decaminute, AVG(latency) AS latency_average, count(latency) AS ping_counts, 
+    dest, SUM(was_it_ok_not_corrupt)/COUNT(was_it_ok_not_corrupt) AS success_rate 
+    FROM pings GROUP BY decaminute, dest),
+limited_to_5_worst AS (SELECT *, row_number() OVER (PARTITION BY dest ORDER BY success_rate) AS group_rn FROM stats_decaminutes)
+SELECT * FROM candidates can JOIN limited_to_5_worst sd ON can.dest = sd.dest WHERE ping_counts > 10 AND group_rn < 5 ORDER BY success_rate;
