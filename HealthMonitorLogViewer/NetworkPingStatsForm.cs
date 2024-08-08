9394d97bb2584d7e467dadcb6765a83611eb4c7e
rishi-on-desktop <oss@rishikeshan.com>
2024-08-08 13:33:06 +0000

    Success rates
    FossilOrigin-Name: 177f240250d496af4169a08d570f7a134cee1289bf82f54b443c9a5178d9c7ec

--- HealthMonitorLogViewer/NetworkPingStatsForm.cs
+++ HealthMonitorLogViewer/NetworkPingStatsForm.cs
@@ -19,15 +19,23 @@ namespace HealthMonitor
         public string Decaminute;
         public double? LatencyAverage;
     }
+    public class PingSuccessRateByDecaminute
+    {
+        public string Decaminute;
+        public double? SuccessRate;
+    }
     public class NetworkPingStatsForm: Form
     {
         public NetworkPingStatsForm() {
-            ScottPlot.Eto.PlotView etoPlot = new() { Size = new Eto.Drawing.Size(800, 600) };
+            Location = new Eto.Drawing.Point(50,50);
+            ScottPlot.Eto.PlotView etoPlot = new() { Size = new Eto.Drawing.Size(1000, 300) };
+            ScottPlot.Eto.PlotView etoPlotSuccessRates = new() { Size = new Eto.Drawing.Size(1000, 300) };
             etoPlot.Plot.XAxis.LabelStyle(fontSize: 24);
-            //etoPlot.Plot.XAxis.TickLabelStyle(fontSize: 24);
             etoPlot.Plot.YAxis.LabelStyle(fontSize: 24);
-            //etoPlot.Plot.YAxis.TickLabelStyle(fontSize: 24);
             etoPlot.Plot.Legend().FontSize = 24;
+            etoPlotSuccessRates.Plot.XAxis.LabelStyle(fontSize: 24);
+            etoPlotSuccessRates.Plot.YAxis.LabelStyle(fontSize: 24);
+            etoPlotSuccessRates.Plot.Legend().FontSize = 24;
 
             var SaveButton = new Button() { Text = "Save As ..." };
             SaveButton.Click += (e, a) =>
@@ -46,21 +54,20 @@ namespace HealthMonitor
                     MessageBox.Show(E.ToString(), MessageBoxType.Error);
                 }
             };
+
             var ReloadButton = new Button() { Text = "Reload" };
             ReloadButton.Click += (e, a) => {  };
+
             var ResetButton = new Button() { Text = "Reset" };
             ResetButton.Click += (e, a) => { etoPlot.Plot.AxisAuto(); etoPlot.Refresh(); };
+
             var TopStackLayout = new StackLayout() { Items = { null, ResetButton, ReloadButton, SaveButton, null }, Orientation = Eto.Forms.Orientation.Horizontal, Spacing= 20 };
 
-            //etoPlot.Plot.AddSignal(ScottPlot.Generate.Sin());
-            //etoPlot.Plot.AddSignal(ScottPlot.Generate.Cos());
             
-            double[] xs = DataGen.Consecutive(51);
-            double[] sin = DataGen.Sin(51);
             List<String> series;
-            List <PingAverageByDecaminute> pingAveragesByDecaminute =new List<PingAverageByDecaminute>();
             List<String> Decaminutes = new List<string>();
             Dictionary<String, List < PingAverageByDecaminute >> PlotData = new();
+            Dictionary<String, List<PingSuccessRateByDecaminute>> PlotDataSuccessRates = new();
             using (var logsContext = new LogsContext())
             {
                 series = logsContext.Database.SqlQuery<String>($"SELECT DISTINCT(dest) FROM pings").ToList();
@@ -68,36 +75,49 @@ namespace HealthMonitor
             }
             foreach (var item in series)
             {
+                List<PingAverageByDecaminute> pingAveragesByDecaminute = new List<PingAverageByDecaminute>();
+                List<PingSuccessRateByDecaminute> pingSuccessRatesByDecaminute = new List<PingSuccessRateByDecaminute>();
                 using (var logsContext = new LogsContext())
                 {
+                    
                     var GroupedByDecaminute = logsContext.Pings.Where((x) => x.Dest == item).GroupBy((e) => e.TimeNow.Substring(0, 18)).ToList();
                     Decaminutes = GroupedByDecaminute.Select((e) => e.Key).ToList();
                     pingAveragesByDecaminute = GroupedByDecaminute.Select(e => new PingAverageByDecaminute { Decaminute = e.Key, LatencyAverage = e.Average((x) => x.Latency) }).ToList();
+                    pingSuccessRatesByDecaminute = GroupedByDecaminute.Select(e => new PingSuccessRateByDecaminute { Decaminute = e.Key, SuccessRate = e.Average((x) => (x.WasItOkNotCorrupt == 1 || x.DidItSucceed == 1) ? 1 : 0 ) }).ToList();
                 }
                 PlotData.Add(item, pingAveragesByDecaminute);
+                PlotDataSuccessRates.Add(item, pingSuccessRatesByDecaminute);
 
             }
-            //pingAveragesByDecaminute.ForEach((e) => MessageBox.Show($"{e.Dest}, {e.Decaminute}, {e.LatencyAverage}"));
 
-
-            //etoPlot.Plot.AddScatter(xs, sin);
             etoPlot.Plot.XAxis.DateTimeFormat(true);
+            etoPlotSuccessRates.Plot.XAxis.DateTimeFormat(true);
             foreach (var item in series)
             {
                 var p = etoPlot.Plot.AddScatter(PlotData[item].Select(e => (DateTime.Parse(e.Decaminute+"0").ToLocalTime().ToOADate())).ToArray(), PlotData[item].Select(e => e.LatencyAverage??0).ToArray(), label: item);
+                var pSuccessRates = etoPlotSuccessRates.Plot.AddScatter(PlotDataSuccessRates[item].Select(e => (DateTime.Parse(e.Decaminute + "0").ToLocalTime().ToOADate())).ToArray(), PlotDataSuccessRates[item].Select(e => e.SuccessRate * 100 ?? 0).ToArray(), label: item);
                 p.Label = item;
                 p.MarkerSize = 6;
                 p.MarkerLineWidth = 3;
-                
+                pSuccessRates.MarkerSize = 6;
+                pSuccessRates.MarkerLineWidth = 3;
+
             }
+
             etoPlot.Plot.Legend();
-
-
             etoPlot.Plot.Title("Ping stats");
             etoPlot.Plot.XLabel("Date");
-            etoPlot.Plot.YLabel("Ping (ms) [Response Time]");
+            etoPlot.Plot.YLabel("Ping [ms] (Response Time)");
             etoPlot.Refresh();
-            var VerticalStackLayout = new StackLayout() { Items = { TopStackLayout, etoPlot }, Orientation = Eto.Forms.Orientation.Vertical, Spacing = 20 };
+
+            etoPlotSuccessRates.Plot.Legend();
+            etoPlotSuccessRates.Plot.Title("Ping stats");
+            etoPlotSuccessRates.Plot.XLabel("Date");
+            etoPlotSuccessRates.Plot.YLabel("Ping Success Rate [%]");
+            etoPlotSuccessRates.Refresh();
+
+
+            var VerticalStackLayout = new StackLayout() { Items = { TopStackLayout, etoPlot, etoPlotSuccessRates }, Orientation = Eto.Forms.Orientation.Vertical, Spacing = 20 };
             Content = VerticalStackLayout;
             Resizable = false;
 
