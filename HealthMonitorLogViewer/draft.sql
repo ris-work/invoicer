466360c67aaba66651291cd7ab74dad55232d60f
rishi-on-desktop <oss@rishikeshan.com>
2024-08-13 14:19:47 +0000

    Experimental
    FossilOrigin-Name: dcec5f368b7fb146eb65d213607c7edb952ad1c6b71bb614593d30e5e0bef2d5

--- HealthMonitorLogViewer/draft.sql
+++ HealthMonitorLogViewer/draft.sql
@@ -1,12 +1,15 @@
+DROP VIEW stats_decaminute;
+DROP VIEW stats_hourly;
+
 CREATE VIEW stats_decaminute AS
 SELECT decaminute, process_name, avg_working_set, max_working_set_for_one_instance, 
 (CASE WHEN total_time_avg-prev_total_time_avg > 0 THEN total_time_avg-prev_total_time_avg ELSE 0 END) AS cpu_diff,
 unixepoch(decaminute || '0') - unixepoch(prev_decaminute || '0') AS time_diff,
 thread_count FROM (
-SELECT substring(time_now, 1, 15) AS decaminute, process_name, avg(working_set)*count(process_name)/tcbdm.count AS avg_working_set,
+SELECT substring(time_now, 1, 15) AS decaminute, process_name, sum(working_set)/tcbdm.count AS avg_working_set,
 max(working_set) AS max_working_set_for_one_instance,
-avg(total_time)*count(process_name) AS total_time_avg,
-lag(avg(total_time)*count(process_name)) OVER (PARTITION BY process_name ORDER BY time_now) AS prev_total_time_avg,
+sum(total_time) AS total_time_sum,
+lag(sum(total_time)) OVER (PARTITION BY process_name ORDER BY time_now) AS prev_total_time_sum,
 lag(substring(time_now, 1, 13)) OVER (PARTITION BY process_name ORDER BY time_now) AS prev_decaminute,
 avg(thread_count) AS thread_count
 FROM process_history ph JOIN times_collected_by_decaminute tcbdm ON decaminute=tcbdm.time_decaminute 
@@ -18,12 +21,23 @@ SELECT hour, process_name, avg_working_set, max_working_set_for_one_instance,
 (CASE WHEN total_time_avg-prev_total_time_avg > 0 THEN total_time_avg-prev_total_time_avg ELSE 0 END) AS cpu_diff,
 unixepoch(hour || ':00') - unixepoch(prev_hour || ':00') AS time_diff,
 thread_count FROM (
-SELECT substring(time_now, 1, 13) AS hour, process_name, avg(working_set)*count(process_name)/tcbh.count AS avg_working_set,
+SELECT substring(time_now, 1, 13) AS hour, process_name, sum(working_set)/tcbh.count AS avg_working_set,
 max(working_set) AS max_working_set_for_one_instance,
-avg(total_time)*count(process_name) AS total_time_avg,
-lag(avg(total_time)*count(process_name)) OVER (PARTITION BY process_name ORDER BY time_now) AS prev_total_time_avg,
+sum(total_time) AS total_time_sum,
+lag(sum(total_time)) OVER (PARTITION BY process_name ORDER BY time_now) AS prev_total_time_sum,
 lag(substring(time_now, 1, 13)) OVER (PARTITION BY process_name ORDER BY time_now) AS prev_hour,
 avg(thread_count) AS thread_count
 FROM process_history ph JOIN times_collected_by_hour tcbh ON hour=tcbh.time_hour 
 GROUP BY substring(time_now, 1, 13), process_name
 );
+
+CREATE VIEW max_avg_working_set AS 
+SELECT process_name, avg(CAST(working_set AS REAL)) AS avg_working_set_value ,
+max(CAST(working_set AS REAL)) AS max_working_set_value 
+FROM process_history 
+GROUP BY process_name 
+ORDER BY avg_working_set_value DESC;
+
+CREATE VIEW window_titles AS 
+SELECT DISTINCT main_window_title, process_name
+FROM process_history;
