f6d357b0ab033b4dc609606d344b513d6dfc8482
rishi-on-desktop <oss@rishikeshan.com>
2024-08-13 20:06:06 +0000

    Prepare to publish...
    FossilOrigin-Name: afd6233560392ab4a85b5033422c5ac2198a98ff8ac56f28fb3b94e86a9b6b83

--- HealthMonitorLogViewer/LogsContext.cs
+++ HealthMonitorLogViewer/LogsContext.cs
@@ -41,7 +41,7 @@ public partial class LogsContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlite("Data Source=logs.sqlite3");
+        => optionsBuilder.UseSqlite($"Data Source={Config.LogFile}");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
