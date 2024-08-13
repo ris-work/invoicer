8c99075a7da62a6da4cc494f4218d917d2cb9d34
rishi-on-desktop <oss@rishikeshan.com>
2024-08-13 10:50:04 +0000

    Draft query
    FossilOrigin-Name: f2be19427d7e6fec3ea01a3d7f102626430f29e3de27d0e563acc7e0cd10ed3e

---
+++ HealthMonitorLogViewer/draft.sql
@@ -0,0 +1,13 @@
+CREATE VIEW stats_decaminute AS
+SELECT substring(time_now, 1, 15) AS decaminute, process_name, avg(working_set)*count(process_name)/tcbdm.count AS avg_working_set,
+max(working_set) AS max_working_set_for_one_instance,
+avg(thread_count) AS thread_count, 
+FROM process_history ph JOIN times_collected_by_decaminute tcbdm ON decaminute=tcbdm.time_decaminute 
+GROUP BY substring(time_now, 1, 15), process_name;
+
+CREATE VIEW stats_hourly AS 
+SELECT substring(time_now, 1, 13) AS hour, process_name, avg(working_set)*count(process_name)/tcbdm.count AS avg_working_set,
+max(working_set) AS max_working_set_for_one_instance,
+avg(thread_count) AS thread_count, 
+FROM process_history ph JOIN times_collected_by_decaminute tcbdm ON decaminute=tcbdm.time_decaminute 
+GROUP BY substring(time_now, 1, 13), process_name;
