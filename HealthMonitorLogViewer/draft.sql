9989c77e0536694b10f210410d0a2487d3662df1
rishi-on-desktop <oss@rishikeshan.com>
2024-08-13 14:47:47 +0000

    Better views, schema
    FossilOrigin-Name: 6d9d1912c82fe82eeb40fc26de9af2183ff9cd42f35ce752fd928d255f0bbe26

--- HealthMonitorLogViewer/draft.sql
+++ HealthMonitorLogViewer/draft.sql
@@ -12,8 +12,8 @@ unixepoch(decaminute || '0') - unixepoch(prev_decaminute || '0') AS time_diff,
 thread_count FROM (
 SELECT substring(time_now, 1, 15) AS decaminute, process_name, sum(working_set)/tcbdm.count AS avg_working_set,
 max(working_set) AS max_working_set_for_one_instance,
-sum(total_time) AS total_time,
-lag(sum(total_time)) OVER (PARTITION BY process_name ORDER BY time_now) AS prev_total_time,
+sum(total_time)/tcbdm.count AS total_time,
+lag(sum(total_time)/tcbdm.count) OVER (PARTITION BY process_name ORDER BY time_now) AS prev_total_time,
 lag(substring(time_now, 1, 13)) OVER (PARTITION BY process_name ORDER BY time_now) AS prev_decaminute,
 avg(thread_count) AS thread_count
 FROM process_history ph JOIN times_collected_by_decaminute tcbdm ON decaminute=tcbdm.time_decaminute 
@@ -29,8 +29,8 @@ unixepoch(hour || ':00') - unixepoch(prev_hour || ':00') AS time_diff,
 thread_count FROM (
 SELECT substring(time_now, 1, 13) AS hour, process_name, sum(working_set)/tcbh.count AS avg_working_set,
 max(working_set) AS max_working_set_for_one_instance,
-sum(total_time) AS total_time,
-lag(sum(total_time)) OVER (PARTITION BY process_name ORDER BY time_now) AS prev_total_time,
+sum(total_time)/tcbh.count AS total_time,
+lag(sum(total_time)/tcbh.count) OVER (PARTITION BY process_name ORDER BY time_now) AS prev_total_time,
 lag(substring(time_now, 1, 13)) OVER (PARTITION BY process_name ORDER BY time_now) AS prev_hour,
 avg(thread_count) AS thread_count
 FROM process_history ph JOIN times_collected_by_hour tcbh ON hour=tcbh.time_hour 
