c187b696a8422f96e9ded691990137a3d4578670
rishi-on-desktop <oss@rishikeshan.com>
2024-08-13 10:26:18 +0000

    Better args[] processing
    FossilOrigin-Name: 67ed567f72897d75bca83f8dbcdd3b555e566b427db7055bc198083258e89b0a

--- HealthMonitorLogViewer/Program.cs
+++ HealthMonitorLogViewer/Program.cs
@@ -9,7 +9,17 @@ namespace HealthMonitor
         [STAThread]
         static void Main(string[] args)
         {
+            if (args.Length > 0)
+            {
+                Config.LogFile = args[0];
+            }
+            Console.WriteLine($"LogFile: {Config.LogFile}");
             new Application(Eto.Platform.Detect).Run(new MainForm());
         }
     }
+}
+
+public static class Config
+{
+    public static string LogFile = "logs.sqlite3";
 }
