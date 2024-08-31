09211c2a4620768ec327e9b4b2f65c223eaf74eb
rishi-on-desktop <oss@rishikeshan.com>
2024-08-31 10:51:23 +0000

    Minimal working search
    FossilOrigin-Name: edeab2bc3d23c2c780207a4e8a0577d5455047802d1f7952cd243072b01d030b

--- EtoFE/SearchDialog.cs
+++ EtoFE/SearchDialog.cs
@@ -20,13 +20,41 @@ namespace EtoFE
             GridView Results = new GridView();
             Results.Columns.Add(new GridColumn { HeaderText = "Itemcode", DataCell = new TextBoxCell(0) });
             Results.Columns.Add(new GridColumn { HeaderText = "Description", DataCell = new TextBoxCell(1) });
+            Results.Enabled = true;
+            Results.BackgroundColor = Eto.Drawing.Colors.Wheat;
+            Results.Size = new Size(500, 500);
+            Results.CellFormatting += (e, a) => {
+                if (a.Row % 2 == 0)
+                {
+                    a.BackgroundColor = Eto.Drawing.Colors.Turquoise;
+                    a.ForegroundColor = Eto.Drawing.Colors.Black;
+                }
+            };
             TextBox SearchBox = new TextBox();
             MessageBox.Show($"PC: {PC.Count()}");
             bool searching = false;
             List<PosCatalogue> Filtered = new List<PosCatalogue>();
             List<PosCatalogue> FilteredTemp = new List<PosCatalogue>();
             MessageBox.Show($"Last: {PC.Last().itemcode} Desc: {PC.Last().itemdesc}");
+            TL.Padding = 10;
+            TL.Spacing = new Eto.Drawing.Size(10, 10);
             int FilteredCount = 0;
+            Results.GridLines = GridLines.Both;
+            var UpdateView = () => {
+                Filtered = FilteredTemp;
+                List<GridItem> GI = new List<GridItem>();
+                Filtered = FilteredTemp;
+                GI.Add(new GridItem("Hello", "World"));
+                foreach (var item in Filtered)
+                {
+                    GI.Add(new GridItem(item.itemcode.ToString(), item.itemdesc));
+                }
+                //MessageBox.Show(GI.Count().ToString());
+                Results.DataStore = GI;
+                Results.Invalidate(true);
+                this.Invalidate();
+                this.Title = $"Found {FilteredCount} ";
+            };
             SearchBox.KeyDown += (e, a) =>
             {
                 if (SearchBox.Text.Length > 0 && searching != true)
@@ -36,25 +64,18 @@ namespace EtoFE
                     (new Thread(() =>
                     {
                         var FilteredBeforeCounting = PC.AsParallel().Where((x) => x.itemdesc.ToLowerInvariant().Contains(searchString)).AsSequential();
-                        FilteredTemp = FilteredBeforeCounting.Take(100).ToList();
-                        FilteredCount = Filtered.Count();
+                        FilteredTemp = FilteredBeforeCounting.Take(1000).ToList();
+                        FilteredCount = FilteredBeforeCounting.Count();
                         searching = false;
+                        Application.Instance.Invoke(UpdateView);
                         //MessageBox.Show(FilteredCount.ToString());
                     })).Start();
                     
                 }
-                List<GridItem> GI = new List<GridItem>();
-                foreach (var item in FilteredTemp)
-                {
-                    GI.Add(new GridItem(item.itemcode, item.itemdesc) { });
-
-                }
-                Filtered = FilteredTemp;
-                Results.DataStore = Filtered;
-                this.Title = $"Found {FilteredCount} ";
+                
             };
-
             TL.Rows.Add(new TableRow(SearchBox));
+            TL.Rows.Add(new TableRow(LabelResults));
             TL.Rows.Add(new TableRow(Results));
             Content = TL;
         }
