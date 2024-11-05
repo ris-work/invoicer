4ea1f282300fa7f3aca4fe638758fa3c179f3ed1
rishi-on-desktop <oss@rishikeshan.com>
2024-11-05 07:07:01 +0000

    NVR camera
    FossilOrigin-Name: 9d672243fdb764d0cdd01ddbc3957030dc2a5391ccf99ab2ae438d61bc95cf8f

---
+++ others/install-ffmpeg.ps1
@@ -0,0 +1,3 @@
+curl.exe -O "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-n7.1-latest-win64-lgpl-7.1.zip" -L
+mkdir "C:\Program Files\ffmpeg"
+Expand-Archive "ffmpeg-n7.1-latest-win64-lgpl-7.1.zip" -DestinationPath "C:\Program Files\ffmpeg\"
