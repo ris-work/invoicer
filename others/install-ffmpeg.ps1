6040b063f59043e0000c488520b64259f63f9989
rishi-on-desktop <oss@rishikeshan.com>
2024-11-06 02:57:28 +0000

    Upgrade FFMPEG (others)
    FossilOrigin-Name: fdb6cf73713535a8fa2da33c7e3a750efdebc6424f349271c7543f7baefbd1c5

--- others/install-ffmpeg.ps1
+++ others/install-ffmpeg.ps1
@@ -1,3 +1,3 @@
-curl.exe -O "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-n7.1-latest-win64-lgpl-7.1.zip" -L
+curl.exe -O "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-lgpl.zip" -L
 mkdir "C:\Program Files\ffmpeg"
-Expand-Archive "ffmpeg-n7.1-latest-win64-lgpl-7.1.zip" -DestinationPath "C:\Program Files\ffmpeg\"
+Expand-Archive "ffmpeg-master-latest-win64-lgpl.zip" -DestinationPath "C:\Program Files\ffmpeg\"
--- others/stream-desktop-to-nvr.ps1
+++ others/stream-desktop-to-nvr.ps1
@@ -1,7 +1,8 @@
 $terminalname = "terminal"
 $dest_addr_port = "127.0.0.1:554"
+$Env:PATH = "$Env:PATH;C:\Program Files\ffmpeg\ffmpeg-master-latest-win64-lgpl\bin;"
 do {
-    ./ffmpeg -re -f gdigrab -i desktop -pix_fmt yuv420p -vcodec h264 -maxrate 256k -bufsize 2M -f rtsp -vf 'scale=iw*0.5:ih*0.5' -profile:v baseline -preset faster -rtsp_transport tcp -tune zerolatency -g 5 -r 1 "rtsp://$dest_addr_port/screen-$terminalname"
+    ffmpeg -re -f gdigrab -i desktop -pix_fmt yuv420p -vcodec h264 -maxrate 256k -bufsize 2M -f rtsp -vf 'scale=iw*0.5:ih*0.5' -profile:v baseline -preset faster -rtsp_transport tcp -tune zerolatency -g 5 -r 1 "rtsp://$dest_addr_port/screen-$terminalname"
     Start-Sleep 2
 }
 until ($done)
