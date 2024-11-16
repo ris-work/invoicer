2acc9dacf5dde416bac572d3b166491b80dafb72
rishi-on-desktop <oss@rishikeshan.com>
2024-11-16 12:54:11 +0000

    Others: Win32 FFMPEG
    FossilOrigin-Name: ed7566a64fc68da8366befa356c8fa18aec5935d009d0e42340fa44f676b0675

---
+++ others/install-ffmpeg-win32.ps1
@@ -0,0 +1,3 @@
+curl.exe -O "https://github.com/sudo-nautilus/FFmpeg-Builds-Win32/releases/download/latest/ffmpeg-master-latest-win32-lgpl.zip" -L
+mkdir "C:\Program Files\ffmpeg"
+Expand-Archive "ffmpeg-master-latest-win32-lgpl.zip" -DestinationPath "C:\Program Files\ffmpeg\"
