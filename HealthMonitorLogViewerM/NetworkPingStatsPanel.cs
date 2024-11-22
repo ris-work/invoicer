ceaf5cf9f9d95da5a9632c339817d814319c8c93
rishi-on-desktop <oss@rishikeshan.com>
2024-11-22 09:51:57 +0000

    Allow filtering, decaseconds
    FossilOrigin-Name: 4692f66076cee58741a53c13bf75225b34f5b07be5d76c57bb4b6a2545f5863c

--- HealthMonitorLogViewerM/NetworkPingStatsPanel.cs
+++ HealthMonitorLogViewerM/NetworkPingStatsPanel.cs
@@ -33,7 +33,12 @@ namespace HealthMonitor
             etoPlotSuccessRates.Plot.Axes.Bottom.Label.FontSize = 18;
             etoPlotSuccessRates.Plot.Axes.Left.Label.FontSize = 18;
             etoPlotSuccessRates.Plot.Legend.FontSize = 10;
+            string AddressContainsFilter = "";
             //MovableByWindowBackground = true;
+            var ContainsFilterLabel = new Label() { Text = "Contains (case-insensitive) (Press [ENTER])", TextColor = Eto.Drawing.Colors.White };
+            var ContainsFilter = new TextBox() { TextColor = Eto.Drawing.Colors.White, BackgroundColor = Eto.Drawing.Colors.Black, ShowBorder = true };
+            var ContainsFilterLayout = new StackLayout(ContainsFilterLabel, ContainsFilter) { Orientation = Orientation.Vertical, Spacing = 5, HorizontalContentAlignment = HorizontalAlignment.Stretch };
+            
 
             var SaveButton = new Button() { Text = "💾 Save As ..." };
             SaveButton.Click += (e, a) =>
@@ -100,6 +105,7 @@ namespace HealthMonitor
                     ResetButton, 
                     ReloadButton, 
                     SaveButton, 
+                    ContainsFilterLayout,
                     null 
                 }, 
                 Orientation = Eto.Forms.Orientation.Horizontal, 
@@ -112,6 +118,7 @@ namespace HealthMonitor
             List<String> Decaminutes = new List<string>();
             Dictionary<String, List < PingAverageByDecaminute >> PlotData = new();
             Dictionary<String, List<PingSuccessRateByDecaminute>> PlotDataSuccessRates = new();
+            
             using (var logsContext = new LogsContext())
             {
                 series = logsContext.Database.SqlQuery<String>($"SELECT DISTINCT(dest) FROM pings").ToList();
@@ -119,21 +126,67 @@ namespace HealthMonitor
             }
             foreach (var item in series)
             {
-                List<PingAverageByDecaminute> pingAveragesByDecaminute = new List<PingAverageByDecaminute>();
-                List<PingSuccessRateByDecaminute> pingSuccessRatesByDecaminute = new List<PingSuccessRateByDecaminute>();
-                using (var logsContext = new LogsContext())
+                if (item.ToLowerInvariant().Contains(AddressContainsFilter.ToLowerInvariant()))
                 {
-                    
-                    var GroupedByDecaminute = logsContext.Pings.Where((x) => x.Dest == item).GroupBy((e) => e.TimeNow.Substring(0, 18)).ToList();
-                    var GroupedByHour = logsContext.Pings.Where((x) => x.Dest == item).GroupBy((e) => e.TimeNow.Substring(0, 16)).ToList();
-                    Decaminutes = GroupedByDecaminute.Select((e) => e.Key).ToList();
-                    pingAveragesByDecaminute = GroupedByDecaminute.Select(e => new PingAverageByDecaminute { Decaminute = e.Key, LatencyAverage = e.Average((x) => x.Latency) }).ToList();
-                    pingSuccessRatesByDecaminute = GroupedByDecaminute.Select(e => new PingSuccessRateByDecaminute { Decaminute = e.Key, SuccessRate = e.Average((x) => (x.WasItOkNotCorrupt == 1 || x.DidItSucceed == 1) ? 1 : 0 ) }).ToList();
+                    List<PingAverageByDecaminute> pingAveragesByDecaminute = new List<PingAverageByDecaminute>();
+                    List<PingSuccessRateByDecaminute> pingSuccessRatesByDecaminute = new List<PingSuccessRateByDecaminute>();
+                    using (var logsContext = new LogsContext())
+                    {
+
+                        var GroupedByDecaminute = logsContext.Pings.Where((x) => x.Dest == item).GroupBy((e) => e.TimeNow.Substring(0, 18)).ToList();
+                        var GroupedByHour = logsContext.Pings.Where((x) => x.Dest == item).GroupBy((e) => e.TimeNow.Substring(0, 16)).ToList();
+                        Decaminutes = GroupedByDecaminute.Select((e) => e.Key).ToList();
+                        pingAveragesByDecaminute = GroupedByDecaminute.Select(e => new PingAverageByDecaminute { Decaminute = e.Key, LatencyAverage = e.Average((x) => x.Latency) }).ToList();
+                        pingSuccessRatesByDecaminute = GroupedByDecaminute.Select(e => new PingSuccessRateByDecaminute { Decaminute = e.Key, SuccessRate = e.Average((x) => (x.WasItOkNotCorrupt == 1 || x.DidItSucceed == 1) ? 1 : 0) }).ToList();
+                    }
+                    PlotData.Add(item, pingAveragesByDecaminute);
+                    PlotDataSuccessRates.Add(item, pingSuccessRatesByDecaminute);
                 }
-                PlotData.Add(item, pingAveragesByDecaminute);
-                PlotDataSuccessRates.Add(item, pingSuccessRatesByDecaminute);
 
             }
+            var RedrawWithFilter = () => {
+                etoPlot.Plot.Clear();
+                etoPlotSuccessRates.Plot.Clear();
+                foreach (var item in series)
+                {
+
+                    PlotData.Clear();
+                    PlotDataSuccessRates.Clear();
+                    if (item.ToLowerInvariant().Contains(AddressContainsFilter.ToLowerInvariant()))
+                    {
+                        List<PingAverageByDecaminute> pingAveragesByDecaminute = new List<PingAverageByDecaminute>();
+                        List<PingSuccessRateByDecaminute> pingSuccessRatesByDecaminute = new List<PingSuccessRateByDecaminute>();
+                        using (var logsContext = new LogsContext())
+                        {
+
+                            var GroupedByDecaminute = logsContext.Pings.Where((x) => x.Dest == item).GroupBy((e) => e.TimeNow.Substring(0, 18)).ToList();
+                            var GroupedByHour = logsContext.Pings.Where((x) => x.Dest == item).GroupBy((e) => e.TimeNow.Substring(0, 16)).ToList();
+                            Decaminutes = GroupedByDecaminute.Select((e) => e.Key).ToList();
+                            pingAveragesByDecaminute = GroupedByDecaminute.Select(e => new PingAverageByDecaminute { Decaminute = e.Key, LatencyAverage = e.Average((x) => x.Latency) }).ToList();
+                            pingSuccessRatesByDecaminute = GroupedByDecaminute.Select(e => new PingSuccessRateByDecaminute { Decaminute = e.Key, SuccessRate = e.Average((x) => (x.WasItOkNotCorrupt == 1 || x.DidItSucceed == 1) ? 1 : 0) }).ToList();
+                        }
+                        PlotData.Add(item, pingAveragesByDecaminute);
+                        PlotDataSuccessRates.Add(item, pingSuccessRatesByDecaminute);
+                        var p = etoPlot.Plot.Add.SignalXY(PlotData[item].Select(e => (DateTime.Parse(e.Decaminute + "0").ToLocalTime().ToOADate())).ToArray(), PlotData[item].Select(e => e.LatencyAverage ?? 0).ToArray());
+                        var pSuccessRates = etoPlotSuccessRates.Plot.Add.SignalXY(PlotDataSuccessRates[item].Select(e => (DateTime.Parse(e.Decaminute + "0").ToLocalTime().ToOADate())).ToArray(), PlotDataSuccessRates[item].Select(e => e.SuccessRate * 100 ?? 0).ToArray());
+                        p.LegendText = item;
+                        p.MarkerSize = 8;
+                        p.MarkerLineWidth = 8;
+                        ScottPlot.MarkerShape MarkerShapeForItem = PlotUtils.GetRandomMarkerShape();
+                        ScottPlot.LinePattern LinePatternForItem = PlotUtils.GetRandomLinePattern();
+                        p.MarkerShape = MarkerShapeForItem;
+                        p.LinePattern = LinePatternForItem;
+                        pSuccessRates.LegendText = item;
+                        pSuccessRates.MarkerSize = 8;
+                        pSuccessRates.MarkerLineWidth = 8;
+                        pSuccessRates.MarkerShape = MarkerShapeForItem;
+                        pSuccessRates.LinePattern = LinePatternForItem;
+                        etoPlot.Refresh();
+                        etoPlotSuccessRates.Refresh();
+                    }
+
+                }
+            };
 
             etoPlot.Plot.Axes.DateTimeTicksBottom();
             etoPlotSuccessRates.Plot.Axes.DateTimeTicksBottom();
@@ -194,6 +247,14 @@ namespace HealthMonitor
             etoPlotSuccessRates.Plot.Grid.MajorLineColor = ScottPlot.Colors.White;
             etoPlotSuccessRates.Plot.Grid.MinorLineColor = ScottPlot.Colors.DarkGoldenRod;
             etoPlotSuccessRates.Refresh();
+
+            ContainsFilter.KeyUp += (e, a) => {
+                if (ContainsFilter.Text.Length >= 3 || ContainsFilter.Text.Length == 0)
+                {
+                    AddressContainsFilter = ContainsFilter.Text;
+                    RedrawWithFilter();
+                }
+            };
 
 
             var VerticalStackLayout = new StackLayout() { 
