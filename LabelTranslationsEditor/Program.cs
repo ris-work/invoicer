89f3a768a5f642b71742b2192a299b11d8b9c30c
rishi-on-desktop <oss@rishikeshan.com>
2025-02-21 17:53:50 +0000

    Autogenerated UI works now
    FossilOrigin-Name: 414fa6ed897cefcaec1fd4b09730ea4279fbd0305fe538192729b94999a33e9a

--- LabelTranslationsEditor/Program.cs
+++ LabelTranslationsEditor/Program.cs
@@ -30,6 +30,7 @@ class MainForm: Eto.Forms.Form
 {
     public MainForm()
     {
+        var ActionsMap = new Dictionary<string, (ShowAndGetValue, LookupValue)>();
         List<(string, TextAlignment, bool)> HeaderEntries = new()
             {
                 ("Itemcode", TextAlignment.Right, true),
@@ -49,6 +50,22 @@ class MainForm: Eto.Forms.Form
         //new Eto.Forms.Application(Eto.Platforms.Wpf).Run(new SearchDialogEto(SearchCatalogue, HeaderEntries));
         var SearchDialog = new SearchDialogEto(SearchCatalogue, HeaderEntries);
         SearchDialog.ShowModal();
+        (new Form()
+        {
+            Content = new GenEtoUI(
+            SimpleJsonToUISerialization.ConvertToUISerialization(JsonSerializer.Serialize(new { Itemcode = int.Parse(SearchDialog.Selected[0]), English = SearchDialog.Selected[1], Tamil = SearchDialog.Selected[2], Sinhala = SearchDialog.Selected[3] })),
+            (_) =>
+            {
+                return 100;
+            },
+            (_) =>
+            {
+                return 100;
+            },
+            ActionsMap,
+            null
+        ),
+        }).Show();
     }
 
 }
