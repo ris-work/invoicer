60f5bfbf84b49db7951a27aaa2f6c81d13f9f29a
rishi-on-desktop <oss@rishikeshan.com>
2025-02-21 16:59:50 +0000

    Add sample translations (urgent)
    FossilOrigin-Name: 14c8d4388b53293f0bf691d8cb21ab009ee36bdf7863835cbac693a1774e4c25

---
+++ SampleRetrieval/Program.cs
@@ -0,0 +1,45 @@
+﻿using Eto.Forms;
+using RV.LabelRetriever;
+
+Thread.CurrentThread.SetApartmentState(ApartmentState.Unknown);
+Thread.CurrentThread.SetApartmentState(ApartmentState.STA);
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+new Application(Eto.Platforms.Wpf).Run(new MainForm());
+
+public class MainForm : Form
+{
+    public MainForm()
+    {
+        var Btn = new Button() { Text = "See the Message Box" };
+        Btn.Click += (_, _) =>
+        {
+            MessageBox.Show(
+                null,
+                $"{LabelRetriever.LookupTamil(1)}, {LabelRetriever.LookupSinhala(1)}",
+                "Looked up label 1",
+                MessageBoxType.Information
+            );
+            MessageBox.Show(
+                null,
+                $"{LabelRetriever.LookupTamil("003")}, {LabelRetriever.LookupSinhala("0003")}",
+                "Looked up label 3",
+                MessageBoxType.Information
+            );
+            MessageBox.Show(
+                null,
+                $"{LabelRetriever.LookupTamil("1111003")}, {LabelRetriever.LookupSinhala("11110003")}",
+                "Looked up label 3",
+                MessageBoxType.Information
+            );
+        };
+        Content = Btn;
+        Width = 200;
+        Height = 200;
+        WindowStyle = WindowStyle.Utility;
+    }
+}
+
+partial class Program { }
---
+++ i18n_label_retriever/Class1.cs
@@ -0,0 +1,79 @@
+﻿using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace RV.LabelRetriever
+{
+    [JsonSerializable(typeof(ItemLabel))]
+    public class ItemLabel
+    {
+        public int itemcode { get; set; }
+        public string label_i18n_ta { get; set; }
+        public string label_i18n_si { get; set; }
+    }
+
+    public static class LabelRetriever
+    {
+        public static HttpClient HC = new HttpClient();
+        public static List<ItemLabel> LIL = new List<ItemLabel>();
+        public static Dictionary<int, (string Tamil, string Sinhala)> _I18nLabels = new();
+        public static IReadOnlyDictionary<int, (string Tamil, string Sinhala)> I18nLabels;
+        public static bool Initialized = false;
+        public static string username = "i18n_user";
+        public static string password = "i18n_saru";
+
+        public static async Task<int> Initialize()
+        {
+            var byteArray = Encoding.ASCII.GetBytes($"{username}:{password}");
+            HC.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
+                "Basic",
+                Convert.ToBase64String(byteArray)
+            );
+            //Console.WriteLine("Started requesting....");
+            var strResp = HC.GetStringAsync("https://in.test.vz.al/i18n/sample.php")
+                .GetAwaiter()
+                .GetResult();
+            //Console.WriteLine($"strResp: {strResp}");
+            LIL = JsonSerializer.Deserialize<List<ItemLabel>>(strResp);
+            //Console.WriteLine("Started looping....");
+            foreach (ItemLabel IL in LIL)
+            {
+                //System.Console.WriteLine($"{IL.itemcode}, {IL.label_i18n_ta} ");
+                _I18nLabels.Add(IL.itemcode, (IL.label_i18n_ta, IL.label_i18n_si));
+            }
+            I18nLabels = _I18nLabels;
+            Initialized = true;
+            return 0;
+        }
+
+        public static string LookupTamil(string PLU_CODE)
+        {
+            if (!Initialized)
+                Initialize().GetAwaiter().GetResult();
+            return I18nLabels.GetValueOrDefault(int.Parse(PLU_CODE), ("", "")).Item1;
+        }
+
+        public static string LookupTamil(int Itemcode)
+        {
+            if (!Initialized)
+                Initialize().GetAwaiter().GetResult();
+            return I18nLabels.GetValueOrDefault(Itemcode, ("", "")).Item1;
+        }
+
+        public static string LookupSinhala(string PLU_CODE)
+        {
+            if (!Initialized)
+                Initialize().GetAwaiter().GetResult();
+            return I18nLabels.GetValueOrDefault(int.Parse(PLU_CODE), ("", "")).Item2;
+        }
+
+        public static string LookupSinhala(int Itemcode)
+        {
+            if (!Initialized)
+                Initialize().GetAwaiter().GetResult();
+            return I18nLabels.GetValueOrDefault(Itemcode, ("", "")).Item2;
+        }
+    }
+}
