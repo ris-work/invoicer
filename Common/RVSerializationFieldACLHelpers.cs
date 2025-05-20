50701e64a1ddb88b07687faa1a06ef5ff0c00d21
rishi-on-desktop <oss@rishikeshan.com>
2025-05-20 21:05:51 +0000

    Additions, fix T->T patch conversion as it won't work for non-nullables, so make all T->string and (T,string)->T on the server side, csharpier
    FossilOrigin-Name: 89160f2d0cdd3d559181c5bb6c540a60e2a4d7e09883fba8739e270affa4c71d

---
+++ Common/RVSerializationFieldACLHelpers.cs
@@ -0,0 +1,24 @@
+﻿using MyAOTFriendlyExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace common
+{
+    public static class RVSerializationFieldACLHelpers
+    {
+        public static string RemoveRelevantDenyFields<T>(this T Data, string[] DenyList){
+            T _InProgress = Data;
+            string _InProgressChain = Data.ToJson();
+            string DataType = Data.GetType().ToString().ToLowerInvariant();
+            string[] DenyListForAllObjects = DenyList.Where(x => !x.Contains(".")).ToArray();
+            string[] DenyListForSpecificObjectsAndRelevantToThisObject = DenyList.Where(x => x.ToLowerInvariant().Contains($"{DataType}.")).ToArray();
+            foreach (string DenyField in DenyListForAllObjects.Concat(DenyListForSpecificObjectsAndRelevantToThisObject).ToList()) {
+                _InProgressChain = _InProgressChain.RemoveFieldFromJson(DenyField);
+            }
+            return _InProgressChain;
+        }
+    }
+}
