9e7c1f49816921574478d5d638b4ab6b814a0b0d
rishi-on-desktop <oss@rishikeshan.com>
2026-04-28 06:01:37 +0000

    Missing file
    FossilOrigin-Name: 62ebc4d73b50189fdb0477b0aba9e7c8808a7848503a3ba1d9f91034c8194b5f

---
+++ HealthMonitor/HealthMonitor/AllowedKey.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HealthMonitor;
+
+public partial class AllowedKey
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Sha256Fingerprint { get; set; } = null!;
+
+    public string AddedTime { get; set; } = null!;
+
+    public int IsActive { get; set; }
+}
